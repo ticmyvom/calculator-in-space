33a7edd2d0c2d6be4c1c64a7836b7ad0db7ae3cf
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-02-13 18:45:36 -0800

    Add error checking when dividing by zero

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function multiply(n1, n2) {
 }
 
 function divide(n1, n2) {
-    return +n1 / +n2;
+    n2 = +n2;
+    if (n2 === 0) return "lma0: it's undefined when dividing by zero";
+    return +n1 / n2;
 }
