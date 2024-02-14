d492db3edc65476d239b57a69c9b59f54aac665b
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-02-13 18:53:38 -0800

    Add operate()
    This function takes an operator and 2 numbers. Then, it calls the appro-
    priate function depending on the operator passed in earlier.

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ function divide(n1, n2) {
     if (n2 === 0) return "lma0: it's undefined when dividing by zero";
     return +n1 / n2;
 }
+
+let num1,
+    num2,
+    operator;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            console.log(`operate(): ${operator} is undefined.`);
+    }
+}
