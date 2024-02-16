930e534378b1cd390b18605ee0b73cb5ab242a8d
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-02-16 10:48:58 -0800

    Add a parser to get num1, op, num2 from input
    Notes: while `-1 * -4` works, `+1 * -4` doesn't. Possible cause would be
    parseInt(`-1`) return -1 but parseInt(`+1`) returns 1.
    
    A possible fix would be disallowing user the ability to press + for num1

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const display = document.querySelector('#display');
 const ceBtn = document.querySelector('.ce-button');
 const numBtns = document.querySelectorAll('.number-button');
 const opBtns = document.querySelectorAll('.operator-button');
-
-let num1,
-    num2,
-    operator;
+const eqBtn = document.querySelector('.equal-button');
 
 function add(n1, n2) {
     return +n1 + +n2;
@@ -21,7 +18,7 @@ function multiply(n1, n2) {
 
 function divide(n1, n2) {
     n2 = +n2;
-    if (n2 === 0) return "lma0: it's undefined when dividing by zero";
+    if (n2 === 0) return "lma0"; // it's undefined when dividing by zero
     return +n1 / n2;
 }
 
@@ -65,3 +62,26 @@ opBtns.forEach((opBtn) => {
         updateDisplay(opBtn.textContent);
     });
 });
+
+// 6. Make the calculator work!
+eqBtn.addEventListener('click', calculate);
+
+function calculate() {
+    // TODO: enforce button pressed so that the input is valid
+    // invalid input looks like this `123+-*555++++11----11`
+    // valid input looks like this `num op1 op2 num` where op2 can only be `-`
+    let input = display.textContent;
+    input = input.replace(/\s+/g, '');
+    // console.log('before parsing: ', input);
+    
+    let num1,
+        num2,
+        operator;
+
+    num1 = parseInt(input)
+    input = input.replace(String(num1), '');
+    // console.log(input);
+    operator = input[0];
+    num2 = parseInt(input.substring(1));
+    console.log(num1, operator, num2);
+}
