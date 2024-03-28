9c0198455ffd15ed16996c4d1e878a19b63e9590
anhngo <63425188+ticmyvom@users.noreply.github.com>
2024-03-27 20:32:17 -0700

    Add the ability to hit an operator to calculate
    Also, by keeping track of the current states like init, num1, op, and
    num2, I only allow certain buttons to take effect to disallow invalid
    expressions like 01+2 or 1+++++1.

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ const numBtns = document.querySelectorAll('.number-button');
 const opBtns = document.querySelectorAll('.operator-button');
 const eqBtn = document.querySelector('.equal-button');
 
+function createEnum(values) {
+    const enumObject = {};
+    for (const val of values) {
+      enumObject[val] = val;
+    }
+    return Object.freeze(enumObject);
+}
+
+// NOTE: the current setup may make adding a backspace button challenging
+
+// we'll use calcState to determine which button can take effect
+let calcState = createEnum(['init', 'num1', 'op', 'num2']);
+let currentState = calcState.init
+
+function getCalculatorState() {
+    return currentState;
+}
+
+function setCalculatorState(newState) {
+    currentState = newState;
+}
+
+// NOTE: adding a '+' sign before a variable converts it into a number
 function add(n1, n2) {
     return +n1 + +n2;
 }
@@ -26,7 +49,6 @@ function operate(operator, num1, num2) {
     // console.log('operating', num1, operator, num2);
     switch (operator) {
         case "+":
-            console.log(typeof operator);
             return add(num1, num2);
         case "-":
             return subtract(num1, num2);
@@ -35,12 +57,12 @@ function operate(operator, num1, num2) {
         case "/":
             return divide(num1, num2);
         default:
-            // console.log(`operate(): ${operator} is undefined.`);
             return (`${operator} is undefined.`);
         }
 }
 
 ceBtn.addEventListener('click', () => {
+    setCalculatorState(calcState.init);
     display.textContent = '';
 });
 
@@ -50,32 +72,81 @@ function updateDisplay(content) {
 
 numBtns.forEach((numBtn) => {
     numBtn.addEventListener('click', () => {
+        console.log(getCalculatorState());
         // console.log(`clicked ${numBtn.textContent} `);
+
+        if (getCalculatorState() === calcState.op){
+            setCalculatorState(calcState.num2);
+            updateDisplay(numBtn.textContent);
+            return;
+        }
+
+        if  (getCalculatorState() === calcState.init){
+            if (+numBtn.textContent === 0) {
+                // ignore if 0 is pressed in the beginning
+                // we won't evaluate 01 + 2
+                return;
+            }
+            else {
+                setCalculatorState(calcState.num1);
+            }
+        }
         updateDisplay(numBtn.textContent);
     });
 });
 
 opBtns.forEach((opBtn) => {
     opBtn.addEventListener('click', () => {
-        // console.log(`clicked ${opBtn.textContent} `);
-        updateDisplay(opBtn.textContent);
+        console.log(getCalculatorState());
+        let currentOperator = opBtn.textContent;
+
+        // in the beginning, only allow the operator - to be clicked
+        if (getCalculatorState() === calcState.init && currentOperator === '-'){
+            setCalculatorState(calcState.num1);
+            updateDisplay(currentOperator);
+            return;
+        }
+
+        if (getCalculatorState() === calcState.num1){
+            setCalculatorState(calcState.op);
+            updateDisplay(currentOperator);
+            return;
+        }
+
+        // if we're at the op state, only allow clicking + or - operators
+        if (getCalculatorState() === calcState.op){
+            if (currentOperator === '-' || currentOperator === '+'){
+                setCalculatorState(calcState.num2);
+                updateDisplay(currentOperator);
+                return;
+            }
+            else {
+                return; 
+            }
+        }
+
+        if (getCalculatorState() === calcState.num2){
+            // if user finishes choosing the second number and hit an operator button, 
+            // we'll evaluate the current expression
+            // (this disallows expressions like 1+++1****3)
+            let latestChar = display.textContent.trim().slice(-1);
+            if (/\d/.test(latestChar)){
+                calculate(); 
+                updateDisplay(currentOperator);
+                setCalculatorState(calcState.op);
+            }
+            
+            return;
+        }
     });
 });
 
-// 6. Make the calculator work!
 eqBtn.addEventListener('click', calculate);
 
 function calculate() {
-    /**
-     * TODO: enforce button pressed so that the input is valid
-     * invalid input looks like this `123+-*555++++11----11`
-     * valid input looks like this `num op num`
-     * 
-     * +6 + 5 doesn't work => Don't let user press + for num1
-     * 06+5 doesn't work => Don't let user press 0 for num1
-     * 
-     */
-    
+    if (getCalculatorState() === calcState.init){
+        return; // do nothing if = is pressed without any input
+    }
     
     let input = display.textContent;
     input = input.replace(/\s+/g, '');
@@ -90,7 +161,9 @@ function calculate() {
     // console.log(input);
     operator = input[0];
     num2 = parseInt(input.substring(1));
+
     let result = operate(operator, num1, num2);
     display.textContent = result;
+    setCalculatorState(calcState.num1);
     // console.log(num1, operator, num2, '=', result);
 }
